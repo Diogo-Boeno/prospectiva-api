faf23a48cd2da1937ee540a1404a3ff114f1749c
Diogo Boeno <diogoboeno77@gmail.com>
2026-05-16 13:53:03 -0300

    Fix dev.sh

--- dev.sh
+++ dev.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Terminal 1 — backend
 source venv/Scripts/activate
 cd backend
-uvicorn main:app --reload
+uvicorn main:app --reload &
+
+# Terminal 2 — frontend
+cd ../frontend
+python -m http.server 3000
